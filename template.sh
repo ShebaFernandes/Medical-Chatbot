d112d3e657fbc238513018756d7e21820ada8a58
ShebaFernandes <136631361+ShebaFernandes@users.noreply.github.com>
2025-09-06 00:35:45 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,19 @@
+# creating directories
+mkdir -p src
+mkdir -p research
+
+# creating files
+
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch research/analysis.py
+touch requirements.txt
+touch README.md
+
+
+echo "Project structure created successfully."
